8c4e591cafc01d0c8719c9c363e692f0f457d096
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2021-07-13 11:54:42 -0700

    Removes compiled files from GIT.

--- define_crc.js
+++
@@ -1,8 +0,0 @@
-export default function defineCrc(model, calc) {
-  const fn = (value, previous) => calc(value, previous) >>> 0;
-  fn.signed = calc;
-  fn.unsigned = fn;
-  fn.model = model;
-
-  return fn;
-}
